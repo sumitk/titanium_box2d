1b269684803f6d06d9bf0e2ecd8ac42b9a2c86ef
Sébastien Arnaud <arnaudsj@gmail.com>
2011-07-17 20:33:44 -0500

    Updated sample code to working Box2D example: * 1 Filled Red Rectangle * 1 Blue Ball * rotating the device will cause gravity to switch based on orientation

--- example/app.js
+++ example/app.js
@@ -1,27 +1,66 @@
-// This is a test harness for your module
-// You should do something interesting in this harness 
-// to test out the module and to provide instructions 
-// to users on how to use it by example.
-
-
-// open a single window
-var window = Ti.UI.createWindow({
-	backgroundColor:'white'
-});
-var label = Ti.UI.createLabel();
-window.add(label);
+var window = Ti.UI.createWindow();
+var view = Ti.UI.createView();
+window.add(view);
 window.open();
 
-// TODO: write your module tests here
-var box2d = require('ti.box2d');
-Ti.API.info("module is => " + box2d);
+// load the module
+var box2d = require("ti.box2d");
 
-label.text = box2d.example();
+// create the world, using view as the surface
+var world = box2d.createWorld(view);
 
-Ti.API.info("module exampleProp is => " + box2d.exampleProp);
-box2d.exampleProp = "This is a test value";
+// create a block 
+var redBlock = Ti.UI.createView({
+	backgroundColor: "red",
+	width: 50,
+	height: 50,
+	top: 0
+});
 
-if (Ti.Platform.name == "android") {
-	var proxy = box2d.createExample({message: "Creating an example Proxy"});
-	proxy.printMessage("Hello world!");
-}
+var blueBall = Ti.UI.createView({
+	backgroundColor: "blue",
+	borderRadius: 15,
+	width: 30,
+	height: 30,
+	top: 100
+});
+
+// add the block body to the world
+var redBodyRef = world.addBody(redBlock, {
+	density: 12.0,
+	friction: 0.3,
+	restitution: 0.4,
+	type: "dynamic"
+});
+
+// add the ball body to the world
+var blueBodyRef = world.addBody(blueBall, {
+	radius: 15,
+	density: 12.0,
+	friction: 0.3,
+	restitution: 0.4,
+	type: "dynamic"
+});
+
+Ti.Gesture.addEventListener('orientationchange', function(e) {
+	if (e.orientation == Titanium.UI.LANDSCAPE_LEFT) {
+		world.setGravity(9.91, 0);
+	} else if (e.orientation == Titanium.UI.LANDSCAPE_RIGHT) {
+		world.setGravity(-9.91, 0);
+	} else if (e.orientation == Titanium.UI.UPSIDE_PORTRAIT) {
+		world.setGravity(0, 9.91);
+	} else if (e.orientation == Titanium.UI.PORTRAIT) {
+		world.setGravity(0, -9.91);
+	}
+});
+
+world.addEventListener("collision", function(e) {
+	if ((e.a == redBodyRef || e.b == redBodyRef) && e.phase == "begin") {
+		Ti.API.info("the red block collided with something");
+
+		Ti.API.info(JSON.stringify(e));
+	}
+});
+
+// start the world
+world.start();
